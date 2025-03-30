a79984c26afadee893b15ea899f9c445568a550c
farimarwat <farimarwat@gmail.com>
2025-03-30 13:02:43 +0500

    Move default headers to httpclient

--- shared/src/androidMain/kotlin/com/farimarwat/movista/di/HttpClient.android.kt
+++ shared/src/androidMain/kotlin/com/farimarwat/movista/di/HttpClient.android.kt
@@ -3,6 +3,8 @@ package com.farimarwat.movista.di
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.headers
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -13,5 +15,11 @@ actual val httpclient: HttpClient =
                 ignoreUnknownKeys = true
                 isLenient = true
             })
+        }
+        defaultRequest {
+            headers {
+                append("Authorization","Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjgwZGQ0MjE3N2FlMjVmZTg1OGEzNDNiYjJmYmRhNiIsIm5iZiI6MTc0Mjk2NjM1OS43NjgsInN1YiI6IjY3ZTM4ZTU3M2RiZTNhYjFmMmYwNGIyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.APd4JECHic57qaX5vap5B-qGIYMzOB6OfAVRX66Wi2Q")
+                append("accept","application/json")
+            }
         }
     }
--- shared/src/commonMain/kotlin/com/farimarwat/movista/data/remote/MovieApi.kt
+++ shared/src/commonMain/kotlin/com/farimarwat/movista/data/remote/MovieApi.kt
@@ -10,10 +10,6 @@ import io.ktor.client.request.url
 import io.ktor.http.parameters
 
 class MovieApi(private val client: HttpClient) {
-    /* curl --request GET \
-     --url 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1' \
-     --header 'Authorization:
-     --header 'accept: application/json'*/
 
     suspend fun getPopularMovies(): MovieDto {
         return client.get {
@@ -22,10 +18,6 @@ class MovieApi(private val client: HttpClient) {
                 append("language","en-US")
                 append("page","1")
             }
-            headers {
-                append("Authorization","Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjgwZGQ0MjE3N2FlMjVmZTg1OGEzNDNiYjJmYmRhNiIsIm5iZiI6MTc0Mjk2NjM1OS43NjgsInN1YiI6IjY3ZTM4ZTU3M2RiZTNhYjFmMmYwNGIyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.APd4JECHic57qaX5vap5B-qGIYMzOB6OfAVRX66Wi2Q")
-                append("accept","application/json")
-            }
         }.body<MovieDto>()
     }
     suspend fun getTrendingMovies():MovieDto{
@@ -33,10 +25,6 @@ class MovieApi(private val client: HttpClient) {
             url("https://api.themoviedb.org/3/trending/movie/day")
             parameters {
                 append("language","en-US")
-            }
-            headers {
-                append("Authorization","Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjgwZGQ0MjE3N2FlMjVmZTg1OGEzNDNiYjJmYmRhNiIsIm5iZiI6MTc0Mjk2NjM1OS43NjgsInN1YiI6IjY3ZTM4ZTU3M2RiZTNhYjFmMmYwNGIyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.APd4JECHic57qaX5vap5B-qGIYMzOB6OfAVRX66Wi2Q")
-                append("accept","application/json")
             }
         }.body<MovieDto>()
     }
@@ -47,10 +35,6 @@ class MovieApi(private val client: HttpClient) {
             parameters {
                 append("language","en-US")
                 append("page","1")
-            }
-            headers {
-                append("Authorization","Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YjgwZGQ0MjE3N2FlMjVmZTg1OGEzNDNiYjJmYmRhNiIsIm5iZiI6MTc0Mjk2NjM1OS43NjgsInN1YiI6IjY3ZTM4ZTU3M2RiZTNhYjFmMmYwNGIyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.APd4JECHic57qaX5vap5B-qGIYMzOB6OfAVRX66Wi2Q")
-                append("accept","application/json")
             }
         }.body<SeriesDto>()
     }
